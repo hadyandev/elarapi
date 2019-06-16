38501e9f3e0ebf30a936747068c9fcd641ed5d3b
Hadyan Ahmad <hadyandev@gmail.com>
2019-06-16 22:33:29 +0700

    Seeding for product and review using faker

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(1000, 100000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2, 30),
     ];
 });
