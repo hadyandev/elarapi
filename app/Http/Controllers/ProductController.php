27bca0f0c3ed0bba91b03c0c6525c6a6faf11ef0
Hadyan Ahmad <hadyandev@gmail.com>
2019-06-17 16:25:02 +0700

    Authorize Update and Delete only for Product Owner

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ProductNotBelongsToUser;
 use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
@@ -54,6 +56,7 @@ class ProductController extends Controller
         $product->price = $request->price;
         $product->stock = $request->stock;
         $product->discount = $request->discount;
+        $product->user_id = Auth::id();
         $product->save();
 
         return response([
@@ -93,6 +96,9 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
+        // only authorized user(product owner) can update product
+        $this->ProductUserCheck($product);
+
         $request['detail'] = $request->description;
         unset($request['description']);
         $product->update($request->all());
@@ -110,8 +116,18 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
+        // only authorized user(product owner) can delete product
+        $this->ProductUserCheck($product);
+
         $product->delete();
 
         return response(null, Response::HTTP_NO_CONTENT);
+    }
+
+    public function ProductUserCheck($product)
+    {
+        if (Auth::id() !== $product->user_id) {
+            throw new ProductNotBelongsToUser;
+        }
     }
 }
