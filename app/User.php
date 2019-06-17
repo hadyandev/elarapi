9ac5e8e4d6222f702134d733de8f6c0ba8a2ba6c
Hadyan Ahmad <hadyandev@gmail.com>
2019-06-17 10:31:25 +0700

    Update HasApiTokens import url

--- app/User.php
+++ app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Passport\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
